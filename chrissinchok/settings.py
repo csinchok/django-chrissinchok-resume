04a30f32fdadac57f3bcf749d5a025d92e6a1f74
Chris Sinchok <chris@sinchok.com>
2011-02-03 19:59:09 -0500

    Added sites

--- chrissinchok/settings.py
+++ chrissinchok/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.admin',
+    'django.contrib.sites',
     'south',
     'mptt',
     'resume'
